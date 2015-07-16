a7a26e5467baad28a8bc9e44c6c1fa6ea5fbb0a6
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-07-16 07:15:23 -0600

    Begin refactoring game for easier console use

--- game.js
+++ game.js
@@ -1,32 +1,34 @@
-console.log('Welcome to Cee-Lo');
+// This is our version of 'cee-lo'.
 
-var die = 0;
-var player1 = [], computer = [];
-roll(computer);
-roll(player1);
-
-function sum(array) {
-  var count = 0;
-  for (var i = 0; i < array.length; i++) {
-    count = count + array[i];
-  }
-  return count;
-}
+var player = [], computer = [];
 
 var ceeLo = {
-  rollFor: function roll(player) {
-    for (var i = 0; i < 3; i++) {
-      die = Math.ceil(Math.random() * 6);
-      player.push(die);
-    }
-      console.log(player);
-  },
+    reset: function() {
+        player = [], computer = [];
+        return console.log('Welcome to Cee-lo');
+    },
 
-  compare: function compare() {
-    if (sum(player1) > sum(computer)) {
-      console.log('player1 wins!', player1);
-    } else if (sum(computer) > sum(player1)){
-      console.log('computer wins :(', computer);
+    roll: function roll (player) {
+        var die = 0;
+        for (var i = 0; i < 3; i++) {
+            die = Math.ceil(Math.random() * 6);
+            player.push(die);
+        }
+    },
+
+  compare: function() {
+    var turn = 'Type ceeLo.reset() to play again';
+    var sum = function(side) {
+      var sideSum = 0;
+      for (var i = 0; i < side.length; i++) {
+        sideSum = sideSum + side[i];
+      }
+      return sideSum;
+    };
+    if (sum(player) > sum(computer)) {
+      console.log('player wins!', player + ' > ' + computer + '\n' + turn);
+    } else {
+      console.log('computer wins :(', computer + ' > ' + player + '\n' + turn);
     }
   }
 };
