3b126294b4ed6ab0f114e7d6dcba05fdfd667efb
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-07-23 14:19:59 -0600

    Refactor JS for DOM and Logic separation

--- dom-buttons.js
+++ dom-buttons.js
@@ -1,16 +1,17 @@
 // Reset button
-var reset = document.getElementById('reset');
-reset.addEventListener('click', function() {
-  ceeLo.reset();
-  ceeLo.roll(computer);
-  console.log('Game has been reset!');
-});
+  var reset = document.getElementById('reset');
+  reset.addEventListener('click', function() {
+    ceeLo.reset();
+    ceeLo.roll(computer);
+    console.log('Game has been reset!');
+  });
 
 // Compare button
 var compare = document.getElementById('compare');
 compare.addEventListener('click', function() {
   ceeLo.compare();
   if (player.length > 0) {
+    ceeLo.displayDice(computer, computerBox);
     console.log('Rolls have been compared!');
   } else {
     console.log('You have not rolled yet.');
@@ -21,6 +22,12 @@ compare.addEventListener('click', function() {
 var roll = document.getElementById('roll');
 roll.addEventListener('click', function() {
   ceeLo.roll(player);
+  ceeLo.displayDice(player, playerBox);
 });
 
+// DOM variables
 
+var playerBox = document.getElementById('player-hand');
+var computerBox = document.getElementById('computer-hand');
+var p = document.createElement('p');
+var textnode;
--- game.js
+++ game.js
@@ -1,12 +1,16 @@
-
+// Initial game set-up
+window.onload = function() {
+  ceeLo.reset();
+  console.log('test');
+}
 var player = [], computer = [];
 var dice = [
-  { face: '&#x2680', value: 1 },
-  { face: '&#x2681', value: 2 },
-  { face: '&#x2682', value: 3 },
-  { face: '&#x2683', value: 4 },
-  { face: '&#x2684', value: 5 },
-  { face: '&#x2685', value: 6 }
+  { face: '\u2680', value: 1 },
+  { face: '\u2681', value: 2 },
+  { face: '\u2682', value: 3 },
+  { face: '\u2683', value: 4 },
+  { face: '\u2684', value: 5 },
+  { face: '\u2685', value: 6 }
   ];
 
 // Create modular ceeLo functions
@@ -14,16 +18,18 @@ var ceeLo = {
   reset: function() {
     player = [];
     computer = [];
+    ceeLo.roll(computer);
+    playerBox.innerHTML = '';
+    computerBox.innerHTML = '';
     return console.log('Welcome to Cee-lo');
   },
 
   roll: function(player) {
     var die = 0;
-    for (var i = 0; i < 3; i++) {
-      die = Math.ceil(Math.random() * dice.length - 1);
-      player.push(dice[die]);
-    }
-    console.log('player has rolled ' + player);
+      for (var i = 0; i < 3; i++) {
+        die = Math.ceil(Math.random() * dice.length - 1);
+        player.push(dice[die]);
+      }
   },
 
   compare: function() {
@@ -35,13 +41,19 @@ var ceeLo = {
         }
         return sideSum;
       };
-      if (sum(player) > sum(computer)) {
-        console.log('player wins!', player + ' ' + computer + '\n' + turn);
-      } else {
-        console.log('computer wins :(', computer + ' ' + player + '\n' + turn);
-      }
+    if (sum(player) > sum(computer)) {
+      console.log('player wins!', player + ' ' + computer + '\n' + turn);
+    } else {
+      console.log('computer wins :(', computer + ' ' + player + '\n' + turn);
+    }
+  },
+
+  displayDice: function(player, playerSide) {
+    for (var i = 0; i < player.length; i++) {
+      textnode = document.createTextNode(player[i].face);
+      p.appendChild(textnode);
+      playerSide.appendChild(p);
+    }
   }
 };
-//Initialize game
-ceeLo.reset();
 
