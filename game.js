567ecf600d8c887194e410ce96fd6fcfe2e95255
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-07-23 11:57:42 -0600

    Update compare and roll functions for object

--- game.js
+++ game.js
@@ -1,7 +1,12 @@
 
 var player = [], computer = [];
-var dieFace = [
-  '&#x2680','&#x2681','&#x2682','&#x2683','&#x2684','&#x2685'
+var dice = [
+  { face: '&#x2680', value: 1 },
+  { face: '&#x2681', value: 2 },
+  { face: '&#x2682', value: 3 },
+  { face: '&#x2683', value: 4 },
+  { face: '&#x2684', value: 5 },
+  { face: '&#x2685', value: 6 }
   ];
 
 // Create modular ceeLo functions
@@ -15,18 +20,18 @@ var ceeLo = {
   roll: function(player) {
     var die = 0;
     for (var i = 0; i < 3; i++) {
-      die = Math.ceil(Math.random() * 6);
-      player.push(die);
+      die = Math.ceil(Math.random() * dice.length - 1);
+      player.push(dice[die]);
     }
     console.log('player has rolled ' + player);
   },
 
   compare: function() {
     var turn = 'Please press Reset to play again';
-      var sum = function(side) {
+      var sum = function(array) {
         var sideSum = 0;
-        for (var i = 0; i < side.length; i++) {
-          sideSum = sideSum + side[i];
+        for (var i = 0; i < array.length; i++) {
+          sideSum = sideSum + array[i].value;
         }
         return sideSum;
       };
@@ -37,7 +42,6 @@ var ceeLo = {
       }
   }
 };
-
 //Initialize game
 ceeLo.reset();
 
